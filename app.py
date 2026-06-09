fe156ba15eb3dbac529936ac24e4889c42cf1ebb
Pavan Kumar Srk <pavankumar.srk24@gmail.com>
2026-06-09 20:13:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="InsightLens AI",
+    page_icon="🔍",
+    layout="wide"
+)
+
+#st.sidebar.markdown("# 🔍 InsightLens AI")
+
+st.title(
+    "🔍 InsightLens AI"
+)
+
+st.markdown(
+    """
+    ## Visual Intelligence Assistant
+
+    Welcome to InsightLens AI.
+
+    Navigate using the sidebar.
+
+    ### Features
+
+    - Multimodal AI
+    - Gemini Vision
+    - Image Question Answering
+    - History Tracking
+    - Download Responses
+    """
+)
